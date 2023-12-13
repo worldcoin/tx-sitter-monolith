41fd8af3bfaeba250026452ba28519b8d93dd728
Jakub Trąd <jakubtrad@gmail.com>
2023-12-13 16:17:23 +0100

    Preconfigured start - for dockerization (#6)
    * Allow preconfigured networks & relayers
    
    * Only allow single network and relayer

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -18,10 +18,9 @@ use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::EnvFilter;
 use tx_sitter::client::TxSitterClient;
 use tx_sitter::config::{
-    Config, DatabaseConfig, KeysConfig, LocalKeysConfig, ServerConfig,
-    TxSitterConfig,
+    Config, DatabaseConfig, KeysConfig, LocalKeysConfig, Predefined,
+    PredefinedNetwork, PredefinedRelayer, ServerConfig, TxSitterConfig,
 };
-use tx_sitter::server::routes::network::NewNetworkInfo;
 use tx_sitter::service::Service;
 
 pub type AppMiddleware = SignerMiddleware<Arc<Provider<Http>>, LocalWallet>;
@@ -49,11 +48,17 @@ pub const DEFAULT_ANVIL_PRIVATE_KEY: &[u8] = &hex_literal::hex!(
     "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 );
 
+pub const SECONDARY_ANVIL_PRIVATE_KEY: &[u8] = &hex_literal::hex!(
+    "59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"
+);
+
 pub const ARBITRARY_ADDRESS: Address = H160(hex_literal::hex!(
     "1Ed53d680B8890DAe2a63f673a85fFDE1FD5C7a2"
 ));
 
 pub const DEFAULT_ANVIL_CHAIN_ID: u64 = 31337;
+
+pub const DEFAULT_RELAYER_ID: &str = "1b908a34-5dc1-4d2d-a146-5eb46e975830";
 
 pub struct DoubleAnvilHandle {
     pub double_anvil: Arc<DoubleAnvil>,
@@ -104,7 +109,7 @@ pub async fn setup_double_anvil() -> eyre::Result<DoubleAnvilHandle> {
 
     let middleware = setup_middleware(
         format!("http://{local_addr}"),
-        DEFAULT_ANVIL_PRIVATE_KEY,
+        SECONDARY_ANVIL_PRIVATE_KEY,
     )
     .await?;
 
@@ -138,11 +143,27 @@ pub async fn setup_service(
 ) -> eyre::Result<(Service, TxSitterClient)> {
     let rpc_url = anvil_handle.local_addr();
 
+    let anvil_private_key = hex::encode(DEFAULT_ANVIL_PRIVATE_KEY);
+
     let config = Config {
         service: TxSitterConfig {
             escalation_interval,
             datadog_enabled: false,
             statsd_enabled: false,
+            predefined: Some(Predefined {
+                network: PredefinedNetwork {
+                    chain_id: DEFAULT_ANVIL_CHAIN_ID,
+                    name: "Anvil".to_string(),
+                    http_rpc: format!("http://{}", rpc_url),
+                    ws_rpc: anvil_handle.ws_addr(),
+                },
+                relayer: PredefinedRelayer {
+                    name: "Anvil".to_string(),
+                    id: DEFAULT_RELAYER_ID.to_string(),
+                    key_id: anvil_private_key,
+                    chain_id: DEFAULT_ANVIL_CHAIN_ID,
+                },
+            }),
         },
         server: ServerConfig {
             host: SocketAddr::V4(SocketAddrV4::new(
@@ -153,24 +174,13 @@ pub async fn setup_service(
             password: None,
         },
         database: DatabaseConfig::connection_string(db_connection_url),
-        keys: KeysConfig::Local(LocalKeysConfig {}),
+        keys: KeysConfig::Local(LocalKeysConfig::default()),
     };
 
     let service = Service::new(config).await?;
 
     let client =
         TxSitterClient::new(format!("http://{}", service.local_addr()));
-
-    client
-        .create_network(
-            DEFAULT_ANVIL_CHAIN_ID,
-            &NewNetworkInfo {
-                name: "Anvil".to_string(),
-                http_rpc: format!("http://{}", rpc_url),
-                ws_rpc: anvil_handle.ws_addr(),
-            },
-        )
-        .await?;
 
     Ok((service, client))
 }
@@ -179,7 +189,7 @@ pub async fn setup_middleware(
     rpc_url: impl AsRef<str>,
     private_key: &[u8],
 ) -> eyre::Result<AppMiddleware> {
-    let provider = Provider::<Http>::new(rpc_url.as_ref().parse()?);
+    let provider = setup_provider(rpc_url).await?;
 
     let wallet = LocalWallet::from(SigningKey::from_slice(private_key)?)
         .with_chain_id(provider.get_chainid().await?.as_u64());
@@ -187,4 +197,12 @@ pub async fn setup_middleware(
     let middleware = SignerMiddleware::new(Arc::new(provider), wallet);
 
     Ok(middleware)
+}
+
+pub async fn setup_provider(
+    rpc_url: impl AsRef<str>,
+) -> eyre::Result<Provider<Http>> {
+    let provider = Provider::<Http>::new(rpc_url.as_ref().parse()?);
+
+    Ok(provider)
 }
