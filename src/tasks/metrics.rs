683c35cfd51a95604b67873ff8a13c0d8c1a6c53
Jakub Trąd <jakubtrad@gmail.com>
2024-02-29 15:03:29 +0100

    Bring back metrics (#24)
    * Config sanity check
    
    * Add max_queued_txs
    
    * Update metrics
    
    * Cleanup Cargo.toml
    
    * Add log to start service
    
    * Revert "Add max_queued_txs"
    
    This reverts commit 83c517d92b3fdaa5da5afde8df00ca944fce8135.
    
    * Add shutdown listening (non-graceful)
    
    * clippy + fmt

--- src/tasks/metrics.rs
+++ src/tasks/metrics.rs
@@ -15,19 +15,14 @@ pub async fn emit_metrics(app: Arc<App>) -> eyre::Result<()> {
             // TODO: Add labels for env, etc.
             let labels = [("chain_id", chain_id.to_string())];
 
-            metrics::gauge!("pending_txs", stats.pending_txs as f64, &labels);
-            metrics::gauge!("mined_txs", stats.mined_txs as f64, &labels);
-            metrics::gauge!(
-                "finalized_txs",
-                stats.finalized_txs as f64,
-                &labels
-            );
-            metrics::gauge!(
-                "total_indexed_blocks",
-                stats.total_indexed_blocks as f64,
-                &labels
-            );
-            metrics::gauge!("block_txs", stats.block_txs as f64, &labels);
+            metrics::gauge!("pending_txs", &labels)
+                .set(stats.pending_txs as f64);
+            metrics::gauge!("mined_txs", &labels).set(stats.mined_txs as f64);
+            metrics::gauge!("finalized_txs", &labels)
+                .set(stats.finalized_txs as f64);
+            metrics::gauge!("total_indexed_blocks", &labels)
+                .set(stats.total_indexed_blocks as f64);
+            metrics::gauge!("block_txs", &labels).set(stats.block_txs as f64);
         }
 
         tokio::time::sleep(EMIT_METRICS_INTERVAL).await;
