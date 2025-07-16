8daed549a2493ba6fed955f923200f23c9b4e3a4
Dzejkop <jakubtrad@gmail.com>
2025-07-16 14:31:32 +0200

    add task for monitoring funds of relayers

--- src/tasks.rs
+++ src/tasks.rs
@@ -4,6 +4,7 @@ pub mod finalize;
 pub mod handle_reorgs;
 pub mod index;
 pub mod metrics;
+pub mod monitor_funds;
 pub mod prune;
 
 pub use self::broadcast::broadcast_txs;
@@ -12,4 +13,5 @@ pub use self::finalize::finalize_txs;
 pub use self::handle_reorgs::{handle_hard_reorgs, handle_soft_reorgs};
 pub use self::index::index_chain;
 pub use self::metrics::emit_metrics;
+pub use self::monitor_funds::monitor_funds;
 pub use self::prune::{prune_blocks, prune_txs};
---
+++ src/tasks/monitor_funds.rs
@@ -0,0 +1,50 @@
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::time::{Duration, Instant};
+
+use ethers::providers::Middleware;
+use ethers::types::{NameOrAddress, U256};
+
+use crate::app::App;
+
+const INTERVAL: Duration = Duration::from_secs(15);
+const LOW_BALANCE_THRESHOLD: u64 = 1000000000000000000;
+const REPORTING_INTERVAL: Duration = Duration::from_secs(60);
+
+pub async fn monitor_funds(app: Arc<App>) -> eyre::Result<()> {
+    let low_balance_threshold = U256::from(LOW_BALANCE_THRESHOLD);
+
+    let mut reporting_cache = HashMap::new();
+
+    loop {
+        let relayers = app.db.get_relayers().await?;
+
+        for relayer in &relayers {
+            if let Some(last_time_reported) = reporting_cache.get(&relayer.id) {
+                let diff = Instant::now() - *last_time_reported;
+
+                if diff < REPORTING_INTERVAL {
+                    continue;
+                }
+            }
+            reporting_cache.insert(relayer.id.clone(), Instant::now());
+
+            let provider = app.http_provider(relayer.chain_id).await?;
+
+            let balance = provider
+                .get_balance(NameOrAddress::Address(relayer.address.0), None)
+                .await?;
+
+            if balance < low_balance_threshold {
+                tracing::warn!(
+                    relayer_id = relayer.id,
+                    address = %relayer.address.0,
+                    balance = %balance,
+                    "Relayer has low balance"
+                );
+            }
+        }
+
+        tokio::time::sleep(INTERVAL).await;
+    }
+}
