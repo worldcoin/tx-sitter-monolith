f7948ed5639eb3c5030de11c22a8e36358e9c6f7
Dzejkop <jakubtrad@gmail.com>
2025-07-17 16:23:48 +0200

    sleep -> timer + comments

--- src/tasks/monitor_funds.rs
+++ src/tasks/monitor_funds.rs
@@ -4,6 +4,7 @@ use std::time::{Duration, Instant};
 
 use ethers::providers::Middleware;
 use ethers::types::{NameOrAddress, U256};
+use tokio::time::{self, MissedTickBehavior};
 
 use crate::app::App;
 
@@ -12,14 +13,20 @@ const LOW_BALANCE_THRESHOLD: u64 = 1000000000000000000; // 1 ETH
 const REPORTING_INTERVAL: Duration = Duration::from_secs(60);
 
 pub async fn monitor_funds(app: Arc<App>) -> eyre::Result<()> {
+    let mut timer = time::interval(INTERVAL);
+    timer.set_missed_tick_behavior(MissedTickBehavior::Skip);
+
     let low_balance_threshold = U256::from(LOW_BALANCE_THRESHOLD);
 
     let mut reporting_cache = HashMap::new();
 
     loop {
+        timer.tick().await;
+
         let relayers = app.db.get_relayers().await?;
 
         for relayer in &relayers {
+            // skip if we already reported it in the previous hour
             if let Some(last_time_reported) = reporting_cache.get(&relayer.id) {
                 let diff = Instant::now() - *last_time_reported;
 
@@ -27,6 +34,8 @@ pub async fn monitor_funds(app: Arc<App>) -> eyre::Result<()> {
                     continue;
                 }
             }
+
+            // update cache
             reporting_cache.insert(relayer.id.clone(), Instant::now());
 
             let provider = app.http_provider(relayer.chain_id).await?;
@@ -44,7 +53,5 @@ pub async fn monitor_funds(app: Arc<App>) -> eyre::Result<()> {
                 );
             }
         }
-
-        tokio::time::sleep(INTERVAL).await;
     }
 }
