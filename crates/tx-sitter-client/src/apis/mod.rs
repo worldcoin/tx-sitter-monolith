9e38e91315d68f84addbe9d5d05be127bb09561f
Piotr Heilman <piotr.heilman@toolsforhumanity.com>
2025-07-24 15:05:57 +0200

    Fix fmt.

--- crates/tx-sitter-client/src/apis/mod.rs
+++ crates/tx-sitter-client/src/apis/mod.rs
@@ -82,12 +82,9 @@ pub fn parse_deep_object(
 
         for (key, value) in object {
             match value {
-                serde_json::Value::Object(_) => {
-                    params.append(&mut parse_deep_object(
-                        &format!("{prefix}[{key}]"),
-                        value,
-                    ))
-                }
+                serde_json::Value::Object(_) => params.append(
+                    &mut parse_deep_object(&format!("{prefix}[{key}]"), value),
+                ),
                 serde_json::Value::Array(array) => {
                     for (i, value) in array.iter().enumerate() {
                         params.append(&mut parse_deep_object(
@@ -99,8 +96,9 @@ pub fn parse_deep_object(
                 serde_json::Value::String(s) => {
                     params.push((format!("{prefix}[{key}]"), s.clone()))
                 }
-                _ => params
-                    .push((format!("{prefix}[{key}]"), value.to_string())),
+                _ => {
+                    params.push((format!("{prefix}[{key}]"), value.to_string()))
+                }
             }
         }
 
